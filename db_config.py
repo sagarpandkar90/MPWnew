90d4ef6c56bef84391acd1303a04166628a0ace9
sagar <spandkar90@gmail.com>
2026-01-26 14:38:35 +0530

    db_config updated..

--- db_config.py
+++ db_config.py
@@ -1,16 +1,13 @@
 import psycopg
 import streamlit as st
-
-
 def get_connection():
     return psycopg.connect(
-        conninfo=f"""
-            host={st.secrets['DB_HOST']}
-            port={st.secrets['DB_PORT']}
-            dbname={st.secrets['DB_NAME']}
-            user={st.secrets['DB_USER']}
-            password={st.secrets['DB_PASSWORD']}
-            sslmode=require
-        """,
+        host=st.secrets['DB_HOST'],
+        port=st.secrets['DB_PORT'],
+        dbname=st.secrets['DB_NAME'],
+        user=st.secrets['DB_USER'],
+        password=st.secrets['DB_PASSWORD'],
+        connect_timeout=10,
         autocommit=False
     )
+
